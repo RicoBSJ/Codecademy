8d8d323a3b92ef9b69c992c4d908aa0198f3d543
RicoBSJ <aubrun.eric@free.fr>
2018-12-30 10:59:28 +0100

    4 Commit Home 30_12

---
+++ src/Animal.java
@@ -0,0 +1,7 @@
+
+public class Animal {
+
+	public void checkStatus() {
+		System.out.println("Your pet is healthy and happy!");
+	}
+}
---
+++ src/Dog.java
@@ -0,0 +1,30 @@
+
+public class Dog extends Animal {
+
+	int age;
+
+	public Dog(int dogsAge) {
+		age = dogsAge;
+	}
+
+	public void bark() {
+		System.out.println("Woof !");
+	}
+
+	public void run(int feet) {
+		System.out.println("Your dog ran " + feet + " feet!");
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public static void main(String[] args) {
+		Dog spike = new Dog(3);
+		spike.bark();
+		spike.run(48);
+		int spikeAge = spike.getAge();
+		System.out.println("Spike is " + spikeAge + " years old !");
+		spike.checkStatus();
+	}
+}
